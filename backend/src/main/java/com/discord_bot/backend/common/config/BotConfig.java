60bdbef80d991901de46afe53df02455e7d5c07c
NoRuTnT <moonabcd@naver.com>
2025-11-10 00:09:08 +0900

    feat: 슬래시커맨드 추가

--- backend/src/main/java/com/discord_bot/backend/common/config/BotConfig.java
+++ backend/src/main/java/com/discord_bot/backend/common/config/BotConfig.java
@@ -1,4 +1,4 @@
-package com.discord_bot.backend.config;
+package com.discord_bot.backend.common.config;
 
 import javax.security.auth.login.LoginException;
 
@@ -10,9 +10,11 @@ import org.springframework.context.annotation.Configuration;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
-import com.discord_bot.backend.listener.DiscordListener;
+import com.discord_bot.backend.common.listener.DiscordListener;
 
 @Configuration
 public class BotConfig {
@@ -25,13 +27,25 @@ public class BotConfig {
 
 	@Value("${discord.token}")
 	private String token;
+	private JDA jda;
 
 	@Bean
-	public JDA jdaBuilder() throws LoginException {
-		return JDABuilder.createDefault(token)
-			.setActivity(Activity.playing("라라 대기"))
+	public JDA jdaBuilder() throws LoginException, InterruptedException {
+		jda = JDABuilder.createDefault(token)
+			.setActivity(Activity.playing("라라 대기중..."))
 			.enableIntents(GatewayIntent.MESSAGE_CONTENT)
 			.addEventListeners(context.getBean(DiscordListener.class))
 			.build();
+
+		jda.awaitReady();
+
+		jda.updateCommands().addCommands(
+			Commands.slash("ac-test", "autocomplete test")
+				.addOption(OptionType.STRING, "query", "type...", true, true), // autocomplete=true
+			Commands.slash("주식", "종목 시세 조회")
+				.addOption(OptionType.STRING, "query", "종목명 검색", true, true)
+		).queue();
+
+		return jda;
 	}
 }
